7632ead7032cf6ae37150a637eceb0bd5cf7b3a5
haivan <aivan.cva@gmail.com>
2021-12-21 00:27:26 -0600

    Added sounds funcionality to the buttons

--- src/Components/Drumpad.jsx
+++ src/Components/Drumpad.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const Drumpad = ({ keyCode, keyTrigger, id, url }) => {
+  const playPad = () => {
+    const sound = document.getElementById(id);
+    sound.play();
+  };
+
   return (
-    <div className="drum-pad">
+    <div className="drum-pad" onClick={playPad}>
       <p>{keyTrigger}</p>
-      <audio className="clip" id={id}>
-        <source src={url} />
-      </audio>
+      <audio className="clip" id={id} src={url}></audio>
     </div>
   );
 };
